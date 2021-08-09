ab4a91212aafdec7f19851c29da342069112ecf0
mandha1 <50222776+mandha1@users.noreply.github.com>
2021-08-10 09:55:42 +1200

    unfinished

--- app/src/main/java/op/mobile/app/dev/mandha1/travelling/MainActivity.kt
+++ app/src/main/java/op/mobile/app/dev/mandha1/travelling/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar) // Find the View in activity_main.xml with the id toolbar
         setSupportActionBar(toolbar) // Set toolbar as the entire application's action bar
 
-        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val btmNavView: BottomNavigationView = findViewById(R.id.btm_nav_view)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        btmNavView.setupWithNavController(navHostFragment.navController)
+
     }
 }
---
+++ app/src/main/java/op/mobile/app/dev/mandha1/travelling/ui/home/HomeFragment.kt
@@ -0,0 +1,4 @@
+package op.mobile.app.dev.mandha1.travelling.ui.home
+
+class HomeFragment {
+}
