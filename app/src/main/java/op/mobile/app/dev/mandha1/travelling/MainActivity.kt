87edd2f22e2ee6243128bf7478b185e0d552f25a
mandha1 <50222776+mandha1@users.noreply.github.com>
2021-10-19 09:35:58 +1300

    added nonfunctional nav drawer

--- app/src/main/java/op/mobile/app/dev/mandha1/travelling/MainActivity.kt
+++ app/src/main/java/op/mobile/app/dev/mandha1/travelling/MainActivity.kt
@@ -4,35 +4,59 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.gms.common.util.CollectionUtils.setOf
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        val navController = navHostFragment.navController
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.navigation_login
+//            )
+//        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//
+//        //val toolbar: Toolbar = findViewById(R.id.toolbar) // Find the View in activity_main.xml with the id toolbar
+//        //setSupportActionBar(toolbar) // Set toolbar as the entire application's action bar
+//
+//        //  Allows the bottom navigation to work - going between home, dashboard and notifications fragments
+//        val btmNavView: BottomNavigationView = findViewById(R.id.btm_nav_view)
+//        btmNavView.setupWithNavController(navController)
+
+        //val toolbar: Toolbar = findViewById(R.id.toolbar)
+        //setSupportActionBar(toolbar)
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_login
-            )
-        )
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_dashboard, R.id.navigation_home), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
 
-        //val toolbar: Toolbar = findViewById(R.id.toolbar) // Find the View in activity_main.xml with the id toolbar
-        //setSupportActionBar(toolbar) // Set toolbar as the entire application's action bar
-
-        //  Allows the bottom navigation to work - going between home, dashboard and notifications fragments
-        val btmNavView: BottomNavigationView = findViewById(R.id.btm_nav_view)
-        btmNavView.setupWithNavController(navController)
-
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
