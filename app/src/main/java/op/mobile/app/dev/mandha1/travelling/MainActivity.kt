a8d372fb50e458cf6772bbb59715b0fcee0f247e
mandha1 <50222776+mandha1@users.noreply.github.com>
2021-10-21 09:45:34 +1300

    added buggy persistent night mode on app close

--- app/src/main/java/op/mobile/app/dev/mandha1/travelling/MainActivity.kt
+++ app/src/main/java/op/mobile/app/dev/mandha1/travelling/MainActivity.kt
@@ -1,24 +1,32 @@
 package op.mobile.app.dev.mandha1.travelling
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private var NightMode: Int = 0
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
+
+//    val NightMode: Int
+//    val sharedPreferences: SharedPreferences
+//    var editor: SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -42,6 +50,10 @@ class MainActivity : AppCompatActivity() {
         val value = ai.metaData["keyValue"]
         val key = value.toString()
         Log.d("APIKEY", key)
+
+        sharedPreferences = getSharedPreferences("SharedPrefs", MODE_PRIVATE);
+        NightMode = sharedPreferences.getInt("NightModeInt", 1);
+        AppCompatDelegate.setDefaultNightMode(NightMode);
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -49,5 +61,15 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    //  Allows set theme to persist even when app's closed
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        NightMode = AppCompatDelegate.getDefaultNightMode()
+        sharedPreferences = getSharedPreferences("SharedPrefs", Context.MODE_PRIVATE)
+        editor = sharedPreferences.edit()
+        editor.putInt("NightModeInt", NightMode)
+        editor.apply()
     }
 }
