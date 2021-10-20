9710eaba02e1207087c212dc8ec48f8738500d86
unknown <mandha1@student.op.ac.nz>
2021-10-20 17:06:23 +1300

    fixed nav drawer and registration feature

--- app/src/main/java/op/mobile/app/dev/mandha1/travelling/MainActivity.kt
+++ app/src/main/java/op/mobile/app/dev/mandha1/travelling/MainActivity.kt
@@ -23,32 +23,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val navHostFragment =
-//            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-//        val navController = navHostFragment.navController
-//        val appBarConfiguration = AppBarConfiguration(
-//            setOf(
-//                R.id.navigation_login
-//            )
-//        )
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//
-//        //val toolbar: Toolbar = findViewById(R.id.toolbar) // Find the View in activity_main.xml with the id toolbar
-//        //setSupportActionBar(toolbar) // Set toolbar as the entire application's action bar
-//
-//        //  Allows the bottom navigation to work - going between home, dashboard and notifications fragments
-//        val btmNavView: BottomNavigationView = findViewById(R.id.btm_nav_view)
-//        btmNavView.setupWithNavController(navController)
-
-        //val toolbar: Toolbar = findViewById(R.id.toolbar)
-        //setSupportActionBar(toolbar)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_attractions), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
--- app/src/main/java/op/mobile/app/dev/mandha1/travelling/ui/registration/RegistrationFragment.kt
+++ app/src/main/java/op/mobile/app/dev/mandha1/travelling/ui/registration/RegistrationFragment.kt
@@ -1,63 +1,80 @@
-//package op.mobile.app.dev.mandha1.travelling.ui.registration
-//
-//import android.content.Intent
-//import android.os.Bundle
-//import android.util.Log
-//import android.view.LayoutInflater
-//import android.view.MenuItem
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.Toast
-//import androidx.constraintlayout.helper.widget.MotionEffect.TAG
-//import androidx.databinding.DataBindingUtil
-//import androidx.fragment.app.Fragment
-//import androidx.lifecycle.ViewModelProvider
-//import androidx.navigation.findNavController
-//import com.google.android.gms.common.api.ApiException
-//import com.google.firebase.auth.FirebaseAuth
-//import op.mobile.app.dev.mandha1.travelling.R
-//
-//class RegistrationFragment : Fragment() {
-//
-//    private lateinit var auth: FirebaseAuth
-//
-//    override fun onStart() {
-//        super.onStart()
-//        // Check if user is signed in (non-null) and update UI accordingly.
-//        val currentUser = auth.currentUser
-//        if(currentUser != null){
-//            //reload();
-//        }
-//    }
-//
-//    override fun onCreateView(
-//            inflater: LayoutInflater,
-//            container: ViewGroup?,
-//            savedInstanceState: Bundle?
-//    ): View {
-//
-//        //  FIREBASE AUTH
-//        auth = FirebaseAuth.getInstance()
-//
-//        btnReg.setOnClickListener {
-//
-//            auth.signInWithEmailAndPassword(email, password)
-//                    .addOnCompleteListener(this) { task ->
-//                        if (task.isSuccessful) {
-//                            // Sign in success, update UI with the signed-in user's information
-//                            Log.d(TAG, "signInWithEmail:success")
-//                            val user = auth.currentUser
-//                            updateUI(user)
-//                        } else {
-//                            // If sign in fails, display a message to the user.
-//                            Log.w(TAG, "signInWithEmail:failure", task.exception)
-//                            Toast.makeText(baseContext, "Authentication failed.",
-//                                    Toast.LENGTH_SHORT).show()
-//                            updateUI(null)
-//                        }
-//                    }
-//        }
-//
-//        return binding.root
-//    }
-//}
+package op.mobile.app.dev.mandha1.travelling.ui.registration
+
+import android.os.Bundle
+import android.util.Patterns
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import op.mobile.app.dev.mandha1.travelling.R
+
+class RegistrationFragment: Fragment() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_registration, container, false)
+
+        auth = FirebaseAuth.getInstance()
+
+        val btnRegister: Button = view.findViewById(R.id.btnReg)
+        val etEmailAddress: EditText = view.findViewById(R.id.reg_email)
+        val etPassword: EditText = view.findViewById(R.id.reg_password)
+        val etConfirmPassword: EditText = view.findViewById(R.id.reg_confirm_password)
+
+        btnRegister.setOnClickListener {
+            val email = etEmailAddress.text.toString()
+            val password = etPassword.text.toString()
+            val confirmPassword = etConfirmPassword.text.toString()
+
+            when {
+                email.isEmpty() ->
+                    etEmailAddress.error = "Email is required."
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches() ->
+                    etEmailAddress.error = "Invalid email address format."
+                password.isEmpty() ->
+                    etPassword.error = "Password is required."
+                password.length < 8 ->
+                    etPassword.error = "Password needs to be at least eight characters."
+                password != confirmPassword ->
+                    etConfirmPassword.error = "Passwords do not match."
+                else -> {
+                    register(email, password)
+                }
+            }
+        }
+
+        return view
+    }
+
+    private fun register(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(
+                        activity,
+                        "User created.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    //  Send user to login screen after registering
+                    view?.findNavController()?.navigate(
+                        RegistrationFragmentDirections.actionRegisterFragmentToLoginFragment()
+                    )
+                } else {
+                    Toast.makeText(
+                        activity,
+                        "Email address already exists.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+}
