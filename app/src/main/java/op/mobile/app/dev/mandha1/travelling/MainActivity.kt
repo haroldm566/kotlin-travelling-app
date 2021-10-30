226193297ff3dd3ae96f4f94ded44c8622eaec03
unknown <mandha1@student.op.ac.nz>
2021-10-31 00:39:11 +1300

    fixed main activity layout ordering for displaying drawer and btm nav view, removed google sign in

--- app/src/main/java/op/mobile/app/dev/mandha1/travelling/MainActivity.kt
+++ app/src/main/java/op/mobile/app/dev/mandha1/travelling/MainActivity.kt
@@ -2,13 +2,13 @@ package op.mobile.app.dev.mandha1.travelling
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.content.pm.ApplicationInfo
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.core.view.isVisible
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -17,25 +17,28 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
 
     private var NightMode: Int = 0
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var editor: SharedPreferences.Editor
 
-//    val NightMode: Int
-//    val sharedPreferences: SharedPreferences
-//    var editor: SharedPreferences.Editor
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val btmNavView: BottomNavigationView = findViewById(R.id.btm_nav_view)
 
@@ -43,17 +46,26 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        btmNavView.setupWithNavController(navController)
-
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
 
+        navView.setupWithNavController(navController)
+        btmNavView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
         //  Saves the current theme - light/dark mode
         sharedPreferences = getSharedPreferences("SharedPrefs", MODE_PRIVATE);
         NightMode = sharedPreferences.getInt("NightModeInt", 1);
         AppCompatDelegate.setDefaultNightMode(NightMode);
+
+        //  Hide drawer and bottom nav view layouts if not logged in (unfinished)
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser == null) {
+            btmNavView.isVisible = false
+        }
+        else {
+            Log.d("FIREBASECURRENTUSER", auth.currentUser!!.email.toString())
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -71,5 +83,14 @@ class MainActivity : AppCompatActivity() {
         editor = sharedPreferences.edit()
         editor.putInt("NightModeInt", NightMode)
         editor.apply()
+    }
+
+    //  Closes nav drawer instead of closing the app if the drawer's open when back button's tapped
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
--- app/src/main/java/op/mobile/app/dev/mandha1/travelling/ui/login/LoginFragment.kt
+++ app/src/main/java/op/mobile/app/dev/mandha1/travelling/ui/login/LoginFragment.kt
@@ -1,9 +1,7 @@
 package op.mobile.app.dev.mandha1.travelling.ui.login
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -11,168 +9,71 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
 import op.mobile.app.dev.mandha1.travelling.R
 import op.mobile.app.dev.mandha1.travelling.databinding.FragmentLoginBinding
-import op.mobile.app.dev.mandha1.travelling.login.LoginAdapter
 import op.mobile.app.dev.mandha1.travelling.login.LoginApplication
 import op.mobile.app.dev.mandha1.travelling.model.Login
-import op.mobile.app.dev.mandha1.travelling.ui.countrypage.CountryPageFragmentDirections
 
 class LoginFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
-
-    override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if(currentUser != null){
-            //reload();
-        }
-    }
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
+
         val binding: FragmentLoginBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_login, container, false
         )
 
-        // hide bottom nav if on login screen
-        //val item: MenuItem = menu.findItem(R.id.mobile_navigation)
-
-
         val viewModelFactory =
             LoginViewModelFactory((activity?.applicationContext as LoginApplication).repository)
+
         val viewModel = ViewModelProvider(this, viewModelFactory).get(LoginViewModel::class.java)
 
         binding.lifecycleOwner = viewLifecycleOwner
 
         binding.loginViewModel = viewModel
 
-        binding.rvLoginDetails.adapter = LoginAdapter()
-
         //  FIREBASE AUTH
         auth = FirebaseAuth.getInstance()
 
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
-
-        //val emailAddressEdtTxt: EditText = view.findViewById(R.id.et_email_address)
-        //val passwordEdtTxt: EditText = view.findViewById(R.id.et_password)
-        //val btnLogin: Button = view.findViewById(R.id.btn_login)
-
+        //  Prompt user that they need to fill in the fields if they're empty
+        //      else allow input
         binding.btnLogin.setOnClickListener {
-            val username = binding.etUsername.text.toString().trim()    //  Replace with username / email address
+            val username = binding.etEmailaddr.text.toString().trim()
             val password = binding.etPassword.text.toString().trim()
             when {
-                username.isEmpty() -> binding.etUsername.error = "Please enter a username"
+                username.isEmpty() -> binding.etEmailaddr.error = "Please enter a username"
                 password.isEmpty() -> binding.etPassword.error = "Please enter a password"
                 else -> viewModel.insertLoginDetail(Login(username, password))
             }
 
-            //  Using firebase to log into the app via email and password
-            //e: john.doe@email.com    p: john.doe
+            //  Using Firebase to log into the app via email and password
             auth.signInWithEmailAndPassword(username, password).addOnCompleteListener { task ->
-                if(task.isSuccessful) {
+                if (task.isSuccessful) {
                     Toast.makeText(activity, "Successfully Logged In", Toast.LENGTH_LONG).show()
 
                     val action = LoginFragmentDirections
                         .actionLoginFragmentToHomeFragment()
 
                     view?.findNavController()?.navigate(action)
-                }else {
+                } else {
                     Toast.makeText(activity, "Login Failed", Toast.LENGTH_LONG).show()
                 }
             }
         }
 
-        //  Run the sign-in method for logging in via Google
-        binding.btnGoogleLogin.setOnClickListener {
-            signIn()
-        }
-
-        //  Send user to registration fragment
+        //  Send user to registration fragment if they want to register
         binding.btnRegister.setOnClickListener {
             val action = LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
             it.findNavController().navigate(action)
         }
 
         return binding.root
-    }
-
-    // Prompt the user to sign in using their Google account's email
-    private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
-
-    // Once the user has chosen their Google account's email
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            val exception = task.exception
-
-            // Authenticate with Firebase if Google sign in is successful
-            if (task.isSuccessful) {
-                try {
-                    val account = task.getResult(ApiException::class.java)!!
-                    firebaseAuthWithGoogle(account.idToken!!)
-                    // Catch Google sign in issues
-                } catch (e: ApiException) {
-                    Toast.makeText(
-                        activity,
-                        "Failed to sign in with Google",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-                // Display a message if there is Firebase Authentication issues
-            } else {
-                Toast.makeText(activity, exception.toString(), Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    /**
-     * After a user successfully signs in, get an idToken from the GoogleSignInAccount
-     * object, exchange it for a Firebase credential, and authenticate with Firebase
-     * using the Firebase credential
-     */
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(requireActivity()) {
-                // Navigate to HomeFragment
-                if (it.isSuccessful) {
-                    val action =
-                        LoginFragmentDirections
-                            .actionLoginFragmentToHomeFragment()
-                    view?.findNavController()?.navigate(action)
-                } else {
-                    Toast.makeText(
-                        activity,
-                        "Failed to authenticate with Google",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-    }
-
-    companion object {
-        const val RC_SIGN_IN = 9001
     }
 }
